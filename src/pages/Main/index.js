52fdb52d3fa07a877c12561105d1188b627f4ed2
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-07 15:31:12 -0300

    Added stylized components with styled components to Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form>
+        <input type="text" />
+        <SubmitButton>
+          <FaPlus color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
