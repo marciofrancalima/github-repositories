14ea26f1a9569733a5fb01ccc01738d91e901bac
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-07 20:56:03 -0300

    Added list of repositories, issues and their labels

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #4a90e2;
+  font-size: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #4a90e2;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+    color: #667;
+    line-height: 1.5;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  list-style: none;
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    /* Apply margin top except first element */
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #4a90e2;
+          }
+        }
+
+        span {
+          background: #eee;
+          colot: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+`;
